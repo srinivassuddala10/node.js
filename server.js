d158a13652c71d431ee9945738107ed81ac5517c
srinivassuddala10 <srinivassuddala10@gmail.com>
2025-09-15 12:53:47 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+    // server.js
+    const express = require('express');
+    const app = express();
+    const port = process.env.PORT || 3000;
+
+    app.get('/', (req, res) => {
+        res.send('Hello World from Docker!');
+    });
+
+    app.listen(port, () => {
+        console.log(`Server listening on port ${port}`);
+    });
